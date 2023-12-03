31549d7d01602b335a0806e7d8a7a5ff956e1f20
dengeeks <denis@gmail.com>
2023-12-04 00:47:02 +0600

    added new models replacements

--- breaks/admin.py
+++ breaks/admin.py
@@ -1,12 +1,34 @@
 from django.contrib import admin
-from breaks.models import organizations
+from breaks.models import organizations, groups, replacements
 
 
+#########################################
+# INLINES
+#########################################
+class ReplacementEmployeeInline(admin.TabularInline):
+    model = replacements.ReplacementEmployee
+    fields = ('employee','status')
+
+
+#########################################
+# MODELS
+#########################################
 @admin.register(organizations.Organization)
 class OrganizationAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'director')
 
 
-@admin.register(organizations.Group)
-class OrganizationAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'manager','min_active')
+@admin.register(groups.Group)
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'manager', 'min_active')
+
+
+@admin.register(replacements.Replacement)
+class ReplacementAdmin(admin.ModelAdmin):
+    list_display = ('id', 'group', 'date', 'breaks_start', 'breaks_end', 'breaks_max_duration')
+    inlines = (ReplacementEmployeeInline,)
+
+
+@admin.register(replacements.ReplacementStatus)
+class ReplacementStatusAdmin(admin.ModelAdmin):
+    list_display = ('code', 'name', 'sort', 'is_active')
--- breaks/models/organizations.py
+++ breaks/models/organizations.py
@@ -25,31 +25,3 @@ class Organization(models.Model):
         return f'{self.name} ({self.pk})'
 
 
-class Group(models.Model):
-    organization = models.ForeignKey(
-        'breaks.Organization', models.CASCADE, related_name='groups',
-        verbose_name='Организация',
-    )
-    name = models.CharField('Название', max_length=255)
-    manager = models.ForeignKey(
-        User, models.RESTRICT, related_name='group_managers',
-        verbose_name='Менеджер',
-    )
-    employees = models.ManyToManyField(
-        User, verbose_name='Сотрудники', related_name='group_employees',
-        blank=True,
-    )
-    min_active = models.PositiveSmallIntegerField(
-        'Минимальное количество активных сотрудников', null=True, blank=True,
-    )
-    break_start = models.TimeField('Начало Обеда', null=True, blank=True)
-    break_end = models.TimeField('Конец Обеда', null=True, blank=True)
-    break_max_duration = models.PositiveSmallIntegerField('Максимальное длительность обеда', null=True, blank=True)
-
-    class Meta:
-        verbose_name = 'Группа'
-        verbose_name_plural = 'Группы'
-        ordering = ['name']
-
-    def __str__(self):
-        return f'{self.name} ({self.pk})'
