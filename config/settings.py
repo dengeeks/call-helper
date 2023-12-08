187fa3878bb7a3eb01308d630688224913dd833a
dengeeks <denis@gmail.com>
2023-12-09 00:30:04 +0600

    .

--- config/settings.py
+++ config/settings.py
@@ -17,7 +17,7 @@ import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = [os.environ.get('SECRET_KEY')]
 
 DEBUG = os.environ.get('DEBUG')
 
